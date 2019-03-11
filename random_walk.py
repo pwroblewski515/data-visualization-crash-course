ac4cc943bb9dfad17945423a7d2e4a0734c13bfb
Paul Wroblewski <Paul@Pauls-MacBook-Pro.local>
2019-03-10 20:04:44 -0500

    refactored to add get_step function

--- random_walk.py
+++ random_walk.py
@@ -9,19 +9,20 @@ class RandomWalk():
 		self.x_values = [0]
 		self.y_values = [0]
 
+	def get_step(self):
+		"""Return a random value for the next step"""
+		direction = choice([1, -1])
+		distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8])
+		return direction * distance
+
 	def fill_walk(self):
 		"""Calculate all the points in the walk"""
 
 		# Keep taking steps until the walk reaches the desired length.
 		while len(self.x_values) < self.num_points:
 			# Decide which direction to go and how far to go in that direction
-			x_direction = choice([1, -1])
-			x_distance = choice([0, 1, 2, 3, 4])
-			x_step = x_direction * x_distance
-
-			y_direction = choice([1, -1])
-			y_distance = choice([0, 1, 2, 3, 4])
-			y_step = y_direction * y_distance			
+			x_step = self.get_step()
+			y_step = self.get_step()		
 
 			# Reject moves that go nowhere.
 			if x_step == 0 and y_step == 0:
@@ -33,3 +34,4 @@ class RandomWalk():
 
 			self.x_values.append(next_x)
 			self.y_values.append(next_y)
+
